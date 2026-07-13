63ba27d8a5840c745bd6063e28da688c268bbbf2
Jarif Bhuiyan <jarifog1607@gmail.com>
2026-07-13 00:02:23 -0400

    Describe what you changed

--- Week6/Assignments/Assignment6/GameFiles/Snake.cpp
+++ Week6/Assignments/Assignment6/GameFiles/Snake.cpp
@@ -28,8 +28,26 @@ Snake::~Snake() {
 // - Update `length` (and `tail`/`head` if the snake becomes empty!).
 // - Return the number of segments ACTUALLY killed.
 int Snake::Kill(int count) {
-    // TODO
-    return 0;
+
+    int killed_nodes = 0;
+
+    while (head_ != nullptr && killed_nodes < count) {
+
+        Node* temp = head_;
+
+        head_ = head_->next_;
+
+        delete temp;
+
+        killed_nodes++;
+        length_--;
+    }
+
+    if (head_ == nullptr) {
+        tail_ = nullptr;
+    }
+
+    return killed_nodes;
 }
 
 // TODO: regenerate()
@@ -38,7 +56,29 @@ int Snake::Kill(int count) {
 //   increment `nextId` each time.
 // - Update `tail` (and `head` if the snake was empty!) and `length`.
 void Snake::Regenerate(int count) {
-    // TODO
+
+    for (int i = 0; i < count; i++) {
+
+        Node* newNode = new Node;
+
+        newNode->body_part_ = id_;
+        id_++;
+
+        newNode->next_ = nullptr;
+
+        if (head_ == nullptr) {
+
+            head_ = newNode;
+            tail_ = newNode;
+        }
+        else {
+
+            tail_->next_ = newNode;
+            tail_ = newNode;
+        }
+
+        length_++;
+    }
 }
 
 // TODO: print()
@@ -47,10 +87,31 @@ void Snake::Regenerate(int count) {
 // - If the snake is empty, print something sensible like "Snake: (dead)".
 // - This function must not modify the snake - use a `const Node*` to walk it.
 void Snake::PrintSnake() const {
-    // TODO
+
+    if (head_ == nullptr) {
+        std::cout << "Snake: (dead)\n";
+        return;
+    }
+
+    std::cout << "Snake: ";
+
+    const Node* current = head_;
+
+    while (current != nullptr) {
+
+        std::cout << "[" << current->body_part_ << "]";
+
+        if (current->next_ != nullptr) {
+            std::cout << " -> ";
+        }
+
+        current = current->next_;
+    } //TODO
+
+    std::cout << std::endl;
 }
 
 int Snake::GetLength() const {
     // TODO
-    return 0;
+    return lenmgth_;
 }
